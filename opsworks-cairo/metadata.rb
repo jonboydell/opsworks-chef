a19202e448e4ae64fe182670560792e9552c0f50
Jon Boydell <jon.boydell@akqa.com>
2014-07-02 12:27:10 +0100

    first commit

---
+++ opsworks-cairo/metadata.rb
@@ -0,0 +1,7 @@
+name        "opsworks-cairo"
+description "Extras for cairo node install"
+maintainer  "AKQA"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends "build-essential"
---
+++ opsworks-cairo/recipes/default.rb
@@ -0,0 +1,16 @@
+include_recipe "build-essential::default"
+
+package "libcairo2-dev" do
+end
+
+package "libjpeg8-dev" do
+end
+
+package "libpango1.0-dev" do
+end
+
+package "libgif-dev" do
+end
+
+package "g++" do
+end

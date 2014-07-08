d45fc7cfe04ae55b3cd0b51daabdc388c65c3e25
Jon Boydell <jon.boydell@akqa.com>
2014-07-08 10:28:58 +0100

    identify deploy directory from vars

--- arsenal-bower/recipes/default.rb
+++ arsenal-bower/recipes/default.rb
@@ -1,7 +1,7 @@
 script "install bower" do
 	interpreter "bash"
 	user "root"
-	cwd "/srv/www/arsenal_image_maker/current"
+	cwd node[:deploy]['arsenal_image_maker'][:deploy_to]
 	code <<-EOH
 		npm install -g bower
 		bower install --allow-root --force

358b47a2f8890e39d270a61e99261f1e64fe83c4
Jon Boydell <jon.boydell@akqa.com>
2014-07-08 10:33:50 +0100

    added debug info

--- arsenal-bower/recipes/default.rb
+++ arsenal-bower/recipes/default.rb
@@ -3,7 +3,10 @@ script "install bower" do
 	user "root"
 	cwd node[:deploy]['arsenal_image_maker'][:deploy_to]
 	code <<-EOH
+		echo 'installing to #{node[:deploy]['arsenal_image_maker'][:deploy_to]}'
+		echo 'installing npm'
 		npm install -g bower
+		echo 'installing bower dependencies'
 		bower install --allow-root --force
 	EOH
 end
